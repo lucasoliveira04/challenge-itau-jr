b0d420facf078f6f1bef5baf78654ecdf15496f2
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-04-08 16:06:02 -0300

    test: script de testes unitarios

---
+++ src/test/java/com/challengeitau/challengeitaujunior/ChallengeItauJuniorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challengeitau.challengeitaujunior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeItauJuniorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

c7d9001c221879737196da3cbeedea4edd52b2ad
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-04-08 16:05:39 -0300

    feat : classe principal

---
+++ src/main/java/com/challengeitau/challengeitaujunior/ChallengeItauJuniorApplication.java
@@ -0,0 +1,13 @@
+package com.challengeitau.challengeitaujunior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeItauJuniorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeItauJuniorApplication.class, args);
+    }
+
+}
